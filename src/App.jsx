7403752043e72f7495a750d1b6c98f46ffa4ba4a
GyunHee <ef3232@naver.com>
2023-02-27 16:42:29 +0900

    darkMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import './App.css';
-import { BsSunFill } from 'react-icons/bs';
-import Item from './components/Item';
-import Createitem from './components/Createitem';
+
 import TodoList from './components/TodoList/TodoList';
 import Header from './components/Header/Header';
 import { useState } from 'react';
+import { DarkModeProvider } from './components/context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed'];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header
         filters={filters}
         filter={filter}
         onFilterChange={(filter) => setFilter(filter)}
       />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
---
+++ src/components/context/DarkModeContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    updateDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    // 이전 상태가 다크모드인지 라이트모드인지 기억한다.
+    const isDark =
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+}
+export const useDarkMode = () => useContext(DarkModeContext);
